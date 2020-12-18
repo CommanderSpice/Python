7b5143c4424369bb10986846e5447d315bc5324c
CommanderSpice <commanderspice@braincalibration.de>
2020-12-18 21:10:09 +0100

    Use C++20 designated initializer

--- builder/templates/callback.c
+++ builder/templates/callback.c
@@ -34,33 +34,10 @@ static PyMethodDef callback_methods[] = {
 };
 
 static PyTypeObject PyAICallback_Type = {
-    PyObject_HEAD_INIT(NULL)
-    0,                         /*ob_size*/
-    "PyAICallback",            /*tp_name*/
-    sizeof(PyAICallbackObject),/*tp_basicsize*/
-    0,                         /*tp_itemsize*/
-    0,                         /*tp_dealloc*/
-    0,                         /*tp_print*/
-    0,                         /*tp_getattr*/
-    0,                         /*tp_setattr*/
-    0,                         /*tp_compare*/
-    0,                         /*tp_repr*/
-    0,                         /*tp_as_number*/
-    0,                         /*tp_as_sequence*/
-    0,                         /*tp_as_mapping*/
-    0,                         /*tp_hash */
-    0,                         /*tp_call*/
-    0,                         /*tp_str*/
-    0,                         /*tp_getattro*/
-    0,                         /*tp_setattro*/
-    0,                         /*tp_as_buffer*/
-    Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE, /*tp_flags*/
-    "callback object",           /* tp_doc */
-    0,                         /* tp_traverse */
-    0,                         /* tp_clear */
-    0,                         /* tp_richcompare */
-    0,                         /* tp_weaklistoffset */
-    0,                         /* tp_iter */
-    0,                         /* tp_iternext */
-    callback_methods,             /* tp_methods */
+    {PyObject_HEAD_INIT(NULL)},
+    .tp_name = "PyAICallback",
+    .tp_basicsize = sizeof(PyAICallbackObject),
+    .tp_flags = Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE,
+    .tp_doc = "callback object",
+    .tp_methods = callback_methods,
 };
